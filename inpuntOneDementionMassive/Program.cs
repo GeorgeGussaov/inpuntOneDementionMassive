1e506ecd16fb2a7c11f30b72b395968d94eeb99a
Kasumyan2004@outlook.com <Kasumyan2004@outlook.com>
2022-11-28 10:57:12 +0300

    Добавьте файлы проекта.

---
+++ inpuntOneDementionMassive/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+
+namespace inputOneDementionMassive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] mas = { 5, 8, 1, 9 };
+            foreach (var item in mas)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
