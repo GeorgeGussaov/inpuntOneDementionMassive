d6657ca3ea99d3fe667ac8b917c7e3564bad1b0a
Kasumyan2004@outlook.com <Kasumyan2004@outlook.com>
2022-11-28 11:14:19 +0300

    ввод элементов массива в строку

--- inpuntOneDementionMassive/Program.cs
+++ inpuntOneDementionMassive/Program.cs
@@ -8,13 +8,14 @@ namespace inputOneDementionMassive
     {
         static void Main(string[] args)
         {
-            //ввод элементов массива в столбик
+            //ввод элементов массива в строку
             int n = Convert.ToInt32(Console.ReadLine());
             int[] mas = new int[n];
-
+            string temp = Console.ReadLine();
+            string[] tempMath = temp.Split();
             for (int i = 0; i < n; i++)
             {
-                mas[i] = Convert.ToInt32(Console.ReadLine());
+                mas[i] = Convert.ToInt32(tempMath[i]);
             }
             foreach (var item in mas)
             {
